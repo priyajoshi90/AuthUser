d80b6f4e895bcda3a0defd15fde1e485eb3b32eb
nimeshjohri <nimeshjohri2008@yahoo.com>
2014-10-29 14:37:14 +0530

    minimized sidebar

--- public/dash.js
+++ public/dash.js
@@ -4,9 +4,12 @@ $("#nimesh").click(function(e) {
             e.preventDefault();
             $("#wrapper").toggleClass("toggled");
         });
-    $("#others_link").click(function(){
-        $("#categoryModal").modal('show');
+            $("#others_link").click(function(){
+            $("#categoryModal").modal('show');
     });
+            $("#nimesh").click(function(){
+                $("#duplicate").show();
+            });
  
 $(function()
  {
@@ -33,7 +36,7 @@ $(function()
                 });
             });
 
-  $('#activitydata').click(function(){
+  $('.activitydata').click(function(){
     $.ajax({
         url: '/activities',
         async:false,
@@ -42,41 +45,21 @@ $(function()
         success: function(data)
         {
             active(data);
-            $(".select_all").click(function(){
-    if(
-        $(".select_all").is(":checked"))
-    {
-    $(".dynamo").prop("checked",true);
-    }
-    else
-    {
-    $(".dynamo").prop("checked",false);
-    }
-    });
-    $(".dynamo").click(function(){
-    if($(".dynamo").length==$(".dynamo:checked").length){
-    $(".select_all").prop("checked",true);
-    }
-    else
-    {
-    $(".select_all").prop("checked",false);
-    }
-    });   
         }
     });
   });
   function active(data) {
     $("#some").html("");
-        var r = $("<table class='table table-bordered table-hover'><thead><tr><th><input type='checkbox' class='select_all'>&nbsp;&nbsp;Select</th><th>Activity Name</th><th>Description</th><th>Category</th><th>Status</th><th>Created At</th><th>Last Updated At</th></tr></thead></table>")
+        var r = $("<table class='table table-bordered table-hover'><thead><tr><th>&nbsp;&nbsp;Select</th><th>Activity Name</th><th>Description</th><th>Category</th><th>Status</th><th>Created At</th><th>Last Updated At</th></tr></thead></table>")
        $("#some").append(r);
        for (var i = 0; i <=data.length ; i++) 
      {
-           var tr =$('<tr class="dynamic_row"><td><div class="hello"><input type="checkbox" class="dynamo"</div></td><td>'+(data[i].name)+'</td><td>'+(data[i].desc)+'</td><td>'+(data[i].category.title)+'</td><td>'+(data[i].status.title)+'</td><td>'+(data[i].created_at)+'</td><td>'+(data[i].updated_at)+'</td></tr>')
-       $(".table").append(tr);
+           var trr =$('<tr class="dynamic_row"><td><input type="checkbox" class="dynamo"></td><td>'+(data[i].name)+'</td><td>'+(data[i].desc)+'</td><td>'+(data[i].category.title)+'</td><td>'+(data[i].status.title)+'</td><td>'+(data[i].created_at)+'</td><td>'+(data[i].updated_at)+'</td></tr>')
+       $(".table").append(trr);
      }
-    }; 
-      
- $('#categorydata').click(function(){
+    };
+
+ $('.categorydata').click(function(){
     $.ajax({
         url: '/categories',
         async:false,
@@ -90,7 +73,7 @@ $(function()
   });
    function cate(data) {
     $("#some").html("");
-        var c = $("<table class='table table-bordered table-hover'><thead><tr><th><input type='checkbox' class='select_all'>&nbsp;&nbsp;Select</th><th>Category Name</th><th>Created At</th><th>Last Updated At</th></tr></thead></table>")
+        var c = $("<table class='table table-bordered table-hover'><thead><tr><th>&nbsp;&nbsp;Select</th><th>Category Name</th><th>Created At</th><th>Last Updated At</th></tr></thead></table>")
        $("#some").append(c);
        for (var i = 0; i <=data.length ; i++) 
      {
@@ -107,29 +90,6 @@ $(function()
             }
         });
             });
-
-        $('#activity_form').click(function(){
-        $.ajax({
-        url: '/activities',
-        async:false,
-        type: 'GET',
-        dataType:'json',
-        success: function(data)
-        {
-            dropdown(data);
-        }
-    });
-  });
-        function dropdown(data)
-        {
-            $("#status").html("");
-            for (var i=0;i<=data.length;i++)
-            {
-
-            var d=$("<select id='dynamic_status'><optgroup label='Select Activity'><option>"+(data[i].name)+"</option></optgroup></select>")
-                $("#dynamic_status").append(d);
-            }
-        };
 
     $('.btn-activity').click(function(){
         $.ajax({
